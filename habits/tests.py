603d66b68ac1a8ee46b5fa6ad105d06334ed1230
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2026-07-17 22:43:00 +0700

    add tests

---
+++ habits/tests.py
@@ -0,0 +1,242 @@
+import json
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from users.models import CustomUser
+
+from .models import Habit, Schedule
+
+
+class HabitTestCase(APITestCase):
+    """Группа тестов связанных с обработкой объектов модели Habit"""
+
+    fixtures = ["customuser_fixture.json", "habits_fixture.json", "schedule_fixture.json"]
+
+    def setUp(self) -> None:
+        """Наполнение БД тестовыми данными"""
+
+        self.user = CustomUser.objects.get(email="user_5@mail.py")
+        self.client.force_authenticate(user=self.user)
+
+    def test_habit_creating(self) -> None:
+        """Тест запроса на создание объекта модели Habit"""
+
+        self.assertEqual(len(Habit.objects.all()), 10)
+        url = "/habits/"
+        response = self.client.post(
+            url,
+            data={
+                "place": "new_place",
+                "action": "new_action",
+                "is_pleasant": True,
+                "time_needed": 100,
+                "is_public": False,
+            },
+        )
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(Habit.objects.all()), 11)
+        self.assertEqual(
+            data,
+            {
+                "id": 11,
+                "place": "new_place",
+                "action": "new_action",
+                "is_pleasant": True,
+                "time_needed": 100,
+                "is_public": False,
+                "related_habit": None,
+                "reward": None,
+                "schedule": None,
+            },
+        )
+
+    def test_habit_invalid_creating(self) -> None:
+        """Тест запроса с невалидными данными на создание объекта модели Habit"""
+
+        url = "/habits/"
+        response = self.client.post(
+            url,
+            data={
+                "place": "new_place",
+                "action": "new_action",
+                "is_pleasant": True,
+                "time_needed": 100,
+                "is_public": False,
+                "reward": "new_reward",
+            },
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_getting_own_habits_list(self) -> None:
+        """Тест запроса на отображение списка объектов модели Habit их владельцу"""
+
+        url = "/habits/?owner=True"
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(data["results"]), 4)
+        self.assertEqual(data["next"], None)
+
+    def test_getting_public_habits_list(self) -> None:
+        """Тест запроса на отображение списка объектов модели Habit с положительным признаком публичности"""
+
+        url = "/habits/?public=True&page_size=10"
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(data["results"]), 7)
+        self.assertEqual(data["next"], None)
+
+    def test_not_own_public_habit_retrieve(self) -> None:
+        """Тест запроса на отображение объекта модели Habit с положительным признаком публичности
+        пользователю, не являющемуся его владельцем"""
+
+        habit = Habit.objects.get(action="action_4")
+        url = f"/habits/{habit.pk}/"
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.json(),
+            {
+                "id": habit.pk,
+                "place": "place_4",
+                "action": "action_4",
+                "is_pleasant": True,
+                "related_habit": None,
+                "reward": None,
+                "time_needed": 40,
+                "is_public": True,
+                "schedule": "Каждые 2 часа",
+            },
+        )
+
+    def test_habit_destroy(self) -> None:
+        """Тест успешного запроса на удаление объекта модели Habit его владельцем"""
+
+        self.assertEqual(len(Habit.objects.all()), 10)
+        habit = Habit.objects.get(action="action_10")
+        self.assertEqual(len(Schedule.objects.all()), 6)
+        url = f"/habits/{habit.pk}/"
+        response = self.client.delete(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(len(Habit.objects.all()), 9)
+        self.assertEqual(len(Schedule.objects.all()), 5)
+
+
+class ScheduleTestCase(APITestCase):
+    """Группа тестов связанных с обработкой объектов модели Schedule"""
+
+    fixtures = ["customuser_fixture.json", "habits_fixture.json", "schedule_fixture.json"]
+
+    def setUp(self) -> None:
+        """Наполнение БД тестовыми данными"""
+
+        self.user = CustomUser.objects.get(email="user_4@mail.py")
+        self.client.force_authenticate(user=self.user)
+
+    def test_schedule_creating(self) -> None:
+        """Тест запроса на создание объекта модели Schedule"""
+
+        self.assertEqual(len(Schedule.objects.all()), 6)
+        url = "/schedule/"
+        response = self.client.post(
+            url,
+            data={
+                "name": "Произвольное время",
+                "habit": 6,
+                "type": "times_in_month",
+                "times_in_month": json.dumps([5000, 1000, 10000, 40000, 14900, 43000, 37888, 19999, 25111, 31500]),
+            },
+        )
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(len(Schedule.objects.all()), 7)
+        self.assertEqual(
+            data,
+            {
+                "id": 7,
+                "name": "Произвольное время",
+                "habit": 6,
+                "type": "times_in_month",
+                "times_in_month": [5000, 1000, 10000, 40000, 14900, 43000, 37888, 19999, 25111, 31500],
+                "constant_interval": None,
+                "times_in_week": None,
+            },
+        )
+
+    def test_schedule_invalid_creating(self) -> None:
+        """Тест запроса с невалидными данными на создание объекта модели Schedule"""
+
+        url = "/schedule/"
+        response = self.client.post(
+            url,
+            data={
+                "name": "Произвольное время",
+                "habit": 6,
+                "type": "times_in_month",
+                "times_in_month": [5500, 10000, 40000, 14900, 43000, 37888, 19999, 25111, 31500],
+            },
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual("40060 <= required_value <= 40320" in str(response.json()), True)
+
+    def test_getting_filtered_by_type_schedule_list(self) -> None:
+        """Тест запроса на отображение списка объектов модели Schedule отсортированных по типу расписания"""
+
+        url = "/schedule/?type=times_in_week"
+        response = self.client.get(url)
+        data = response.json()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(data["results"]), 1)
+        self.assertEqual(data["next"], None)
+        self.assertEqual(
+            data["results"][0],
+            {
+                "id": 5,
+                "name": "По будням вечером",
+                "habit": 5,
+                "type": "times_in_week",
+                "constant_interval": None,
+                "times_in_week": [1320, 2760, 4200, 5640, 7080],
+                "times_in_month": None,
+            },
+        )
+
+    def test_schedule_update(self) -> None:
+        """Тест успешного запроса на обновление объекта модели Schedule"""
+
+        habit = Habit.objects.get(action="action_6")
+        self.assertEqual(len(Schedule.objects.filter(habit=habit)), 0)
+        schedule = Schedule.objects.get(name="Каждые 2 часа")
+        url = f"/schedule/{schedule.pk}/"
+        response = self.client.patch(
+            url,
+            data={
+                "habit": habit.pk,
+                "type": "times_in_week",
+                "times_in_week": [120, 240, 360],
+                "constant_interval": None,
+            },
+            format="json",
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        habit_url = f"/habits/{habit.pk}/"
+        response = self.client.get(habit_url)
+        self.assertEqual(
+            response.json(),
+            {
+                "id": 6,
+                "place": "place_6",
+                "action": "action_6",
+                "is_pleasant": False,
+                "related_habit": 2,
+                "reward": None,
+                "time_needed": 60,
+                "is_public": True,
+                "schedule": "Каждые 2 часа",
+            },
+        )
--- habits/validators.py
+++ habits/validators.py
@@ -13,18 +13,22 @@ def validate_month_minutes_list(minutes_list: list) -> list:
     for i in minutes_list:
         if not isinstance(i, int) or not 1 <= i <= 44640:
             raise ValidationError("Все значения в списке times_in_month должны быть целыми числами от 1 до 44640")
-    minutes_list.sort()
-    if minutes_list[0] > 5760:
+    sorted_minutes = sorted(minutes_list)
+    if sorted_minutes[0] > 5760:
         raise ValidationError("Минимальное значение в списке times_in_month не может быть больше 5760")
     last_steady_value = None
-    for j in range(len(minutes_list) - 1):
-        if minutes_list[j + 1] - minutes_list[j] > 10080:
-            raise ValidationError(f"Разница между {minutes_list[j]} и {minutes_list[j + 1]} превышает значение 10080")
-        if minutes_list[j + 1] > 40320 and last_steady_value is None:
-            last_steady_value = minutes_list[j]
+    for j in range(len(sorted_minutes) - 1):
+        if sorted_minutes[j + 1] - sorted_minutes[j] > 10080:
+            raise ValidationError(
+                f"Разница между {sorted_minutes[j]} и {sorted_minutes[j + 1]} превышает значение 10080"
+            )
+        if sorted_minutes[j + 1] > 40320 and last_steady_value is None:
+            last_steady_value = sorted_minutes[j]
     if last_steady_value is None:
-        last_steady_value = minutes_list[-1]
-    if 34560 + minutes_list[0] > last_steady_value:
-        raise ValidationError(f"""При наименьшем значении {minutes_list[0]} в списке times_in_month также обязательно
-        должно быть указано значение, удовлетворяющее условию {34560 + minutes_list[0]} <= required_value <= 40320""")
+        last_steady_value = sorted_minutes[-1]
+    if 34560 + sorted_minutes[0] > last_steady_value:
+        raise ValidationError(
+            f"""При наименьшем значении {sorted_minutes[0]} в списке times_in_month также обязательно
+        должно быть указано значение, удовлетворяющее условию {34560 + sorted_minutes[0]} <= required_value <= 40320"""
+        )
     return minutes_list
