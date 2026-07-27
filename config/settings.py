ce0848219f62affc3788fba65abf7e53451c17af
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2026-07-27 14:01:23 +0700

    fix test-job

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,11 @@ EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 TG_BOT_LINK_HEAD = os.getenv("TG_BOT_LINK", "")
 TG_BOT_ACCESS = os.getenv("TG_BOT_ACCESS")
 
-REDIS_URL = "redis://" + os.getenv("REDIS_HOST", "redis") + ":" + os.getenv("REDIS_PORT", "6379") + "/"
+if TEST_MODE:
+    REDIS_HOST = "localhost"
+else:
+    REDIS_HOST = os.getenv("REDIS_HOST", "redis")
+REDIS_URL = "redis://" + REDIS_HOST + ":" + os.getenv("REDIS_PORT", "6379") + "/"
 
 CACHE_DB = os.getenv("CACHE_DB", "1")
 CACHES = {"default": {"BACKEND": "django.core.cache.backends.redis.RedisCache", "LOCATION": f"{REDIS_URL}{CACHE_DB}"}}
@@ -195,6 +199,5 @@ if TEST_MODE:
             "PORT": "5432",
         }
     }
-    REDIS_HOST = "localhost"
     CSRF_TRUSTED_ORIGINS = ["redis://localhost"]
     CORS_ALLOWED_ORIGINS = ["redis://localhost"]
