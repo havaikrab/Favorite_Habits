d9b00609af9fd3c18604f1a6d572f72f0ea6904e
Горбунов Александр <qwaeszrdxtfc1@yandex.ru>
2026-07-27 13:42:10 +0700

    fix test-job

--- config/settings.py
+++ config/settings.py
@@ -195,6 +195,7 @@ if TEST_MODE:
             "PORT": "5432",
         }
     }
+    REDIS_URL = "redis://localhost"
     CELERY_BROKER_URL = "redis://localhost:6379/2"
     CELERY_RESULT_BACKEND = "redis://localhost:6379/3"
     CSRF_TRUSTED_ORIGINS = ["redis://localhost"]
